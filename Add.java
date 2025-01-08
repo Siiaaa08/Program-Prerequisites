653ba2823e11129caa354930125490504f58aba9
Siiaaa08 <salonibaruaa810@gmail.com>
2025-01-08 11:04:35 -0800

    Add Two Numbers

---
+++ Add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Add{
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the First number");
+	int x = sc.nextInt();
+	System.out.println("Enter the Second number");
+	int y = sc.nextInt();
+	System.out.println(x+y);
+	}
+}	
+	
