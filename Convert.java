8b26860f18b70644c6c27e65d00e67ab74697628
Siiaaa08 <salonibaruaa810@gmail.com>
2025-01-08 13:05:36 -0800

    Convert Kilometers to Miles

---
+++ Convert.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Convert{
+  public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter Distance in kilometeres");
+  double k = sc.nextInt();
+  System.out.println("The Distance in kilometeres is " + (k*0.621371));
+  }
+ } 
